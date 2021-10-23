8241d1559a74fc8629041133086a6630e6c75924
imasshaWe <imasha98.we@gmail.com>
2021-10-23 22:42:48 +0530

    integrate scratch

---
+++ src/main/java/p2p/chat/services/models/Message.java
@@ -0,0 +1,22 @@
+package p2p.chat.services.models;
+
+public abstract class Message {
+    private User sender;
+    private User receiver;
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+}
